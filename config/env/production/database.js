2d143af7f6a7f0082e4b8baa5d2b1ae8d0821b7d
Cairo <cairofelipedev@gmail.com>
2022-07-19 10:14:20 -0300

    Fix bug

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,22 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', env('DATABASE_HOST')),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', env('DATABASE_NAME')),
-        username: env('DATABASE_USERNAME', env('DATABASE_USERNAME')),
-        password: env('DATABASE_PASSWORD', env('DATABASE_PASSWORD')),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "mysql",
+        host: env("DATABASE_HOST", env("DATABASE_HOST")),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", env("DATABASE_NAME")),
+        username: env("DATABASE_USERNAME", env("DATABASE_USERNAME")),
+        password: env("DATABASE_PASSWORD", env("DATABASE_PASSWORD")),
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        },
       },
-      options: {}
+      options: {
+        ssl: env.bool("DATABASE_SSL", false),
+      },
     },
   },
 });
